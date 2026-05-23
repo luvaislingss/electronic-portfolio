36586f7c3ae1e085bf619efb8ac2b0aec15d5e1c
luvaislingss <luvaislingss@users.noreply.github.com>
2026-05-22 22:23:51 -1000

    slides

--- script.js
+++ script.js
@@ -8,3 +8,25 @@ function changeSlide(direction) {
   
   slides[currentSlide].classList.add("active");
 }
+
+let currentSlide = 0;
+const slides = document.querySelectorAll(".slide2");
+
+function changeSlide(direction) {
+  slides[currentSlide].classList.remove("active2");
+  
+  currentSlide = (currentSlide + direction + slides.length) % slides.length;
+  
+  slides[currentSlide].classList.add("active2");
+}
+
+let currentSlide = 0;
+const slides = document.querySelectorAll(".slide3");
+
+function changeSlide(direction) {
+  slides[currentSlide].classList.remove("active3");
+  
+  currentSlide = (currentSlide + direction + slides.length) % slides.length;
+  
+  slides[currentSlide].classList.add("active3");
+}
